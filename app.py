b9bd179456db34c42a44e3ef0aee2a19f198860e
RicardoMartins55 <ricardo.martins.escola@gmail.com>
2022-08-08 22:32:34 +0100

    fixed layout problems

--- app.py
+++ app.py
@@ -74,30 +74,30 @@ def backToHome(vets):
     initialScreen(vets) 
 
 def createNameQuizLayout(vet, vets, full):
-    myFont = font.Font(family='Chaparral Pro', size=18, weight='bold')
+    myFont = font.Font(family='Chaparral Pro', size=int(0.009*window.winfo_screenwidth()), weight='bold')
 
-    frame1 = tk.Frame(window, width=1200, height=600, bg="#222426")
+    frame1 = tk.Frame(window, width=0.625*window.winfo_screenwidth(), height=0.556*window.winfo_screenheight(), bg="#222426")
     frame1.pack()
     frame1.place(anchor='s', relx=0.5, rely=0.63)
 
-    next_button_frame = tk.Frame(window, width=150, height=180,  bd=0)
+    next_button_frame = tk.Frame(window, width=0.078*window.winfo_screenwidth(), height=0.167*window.winfo_screenheight(),  bd=0)
     next_button_frame.place(anchor='s', relx=0.77, rely=0.888)
     next_button_frame.pack_propagate(False)
 
-    next_font=font.Font(family='Chaparral Pro',weight='bold',size=30)
+    next_font=font.Font(family='Chaparral Pro',weight='bold',size=int(0.015*window.winfo_screenwidth()))
     next_button = tk.Button(next_button_frame, text='➔\nNEXT', font=next_font, width=100, height=100, wraplength=130, bg='#424649', fg="white", borderwidth=5)
     next_button.pack()
 
-    menu_button_frame = tk.Frame(window, width=150, height=110, bd=0)
+    menu_button_frame = tk.Frame(window, width=0.078*window.winfo_screenwidth(), height=0.102*window.winfo_screenheight(), bd=0)
     menu_button_frame.place(anchor='s', relx=0.05, rely=0.12)
     menu_button_frame.pack_propagate(False)
 
-    menu_font=font.Font(family='Chaparral Pro', size=25,weight='bold')
+    menu_font=font.Font(family='Chaparral Pro', size=int(0.013*window.winfo_screenwidth()),weight='bold')
     menu_button = tk.Button(menu_button_frame, text='◄\nMENU', font=menu_font, width=100, height=100, wraplength=130, bg='#424649', fg="white", borderwidth=5)
     menu_button.pack()
     
 
-    options = tk.Frame(window, width=800, height=200, bg='#222426')
+    options = tk.Frame(window, width=0.417*window.winfo_screenwidth(), height=0.185*window.winfo_screenheight(), bg='#222426')
     options.pack()
     options.place(anchor='s', relx=0.5, rely=0.9)
 
@@ -107,20 +107,19 @@ def createNameQuizLayout(vet, vets, full):
     options.grid_rowconfigure(0, weight = 1)
     options.grid_rowconfigure(1, weight = 1)
 
-
-    top_left_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    top_left_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     top_left_option.grid(column=0, row=0)
     top_left_option.pack_propagate(False)
 
-    top_right_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    top_right_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     top_right_option.grid(column=1, row=0)
     top_right_option.pack_propagate(False)
 
-    bottom_left_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    bottom_left_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     bottom_left_option.grid(column=0, row=1)
     bottom_left_option.pack_propagate(False)
 
-    bottom_right_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    bottom_right_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     bottom_right_option.grid(column=1, row=1)
     bottom_right_option.pack_propagate(False)
 
@@ -157,10 +156,10 @@ def createNameQuizLayout(vet, vets, full):
 
     options_list = [option1, option2, option3, option4]
 
-    option1.bind('<ButtonRelease>', lambda event, a=option1, b=correct, c=options_list: checkNameAnswer(a, b, c))
-    option2.bind('<ButtonRelease>', lambda event, a=option2, b=correct, c=options_list: checkNameAnswer(a, b, c))
-    option3.bind('<ButtonRelease>', lambda event, a=option3, b=correct, c=options_list: checkNameAnswer(a, b, c))
-    option4.bind('<ButtonRelease>', lambda event, a=option4, b=correct, c=options_list: checkNameAnswer(a, b, c))
+    option1.bind('<ButtonRelease>', lambda event, a=option1, b=correct, c=options_list: checkInstrumentoAnswer(a, b, c))
+    option2.bind('<ButtonRelease>', lambda event, a=option2, b=correct, c=options_list: checkInstrumentoAnswer(a, b, c))
+    option3.bind('<ButtonRelease>', lambda event, a=option3, b=correct, c=options_list: checkInstrumentoAnswer(a, b, c))
+    option4.bind('<ButtonRelease>', lambda event, a=option4, b=correct, c=options_list: checkInstrumentoAnswer(a, b, c))
     next_button.bind('<ButtonRelease>', lambda event, a=full: nextWindowNameQuiz(a))
     menu_button.bind('<ButtonRelease>', lambda event, a=full: backToHome(a))
 
@@ -177,38 +176,38 @@ def createNameQuizLayout(vet, vets, full):
     label.pack()
 
 def createInstrumentQuizLayout(vet, instruments, full):
-    myFont = font.Font(family='Chaparral Pro', size=18, weight='bold')
+    myFont = font.Font(family='Chaparral Pro', size=int(0.009*window.winfo_screenwidth()), weight='bold')
 
-    frame1 = tk.Frame(window, width=1200, height=600, bg="#222426")
+    frame1 = tk.Frame(window, width=0.625*window.winfo_screenwidth(), height=0.556*window.winfo_screenheight(), bg="#222426")
     frame1.pack()
     frame1.place(anchor='s', relx=0.5, rely=0.63)
 
-    next_button_frame = tk.Frame(window, width=150, height=180,  bd=0)
+    next_button_frame = tk.Frame(window, width=0.078*window.winfo_screenwidth(), height=0.167*window.winfo_screenheight(),  bd=0)
     next_button_frame.place(anchor='s', relx=0.77, rely=0.888)
     next_button_frame.pack_propagate(False)
 
-    next_font=font.Font(family='Chaparral Pro',weight='bold',size=30)
+    next_font=font.Font(family='Chaparral Pro',weight='bold',size=int(0.015*window.winfo_screenwidth()))
     next_button = tk.Button(next_button_frame, text='➔\nNEXT', font=next_font, width=100, height=100, wraplength=130, bg='#424649', fg="white", borderwidth=5)
     next_button.pack()
 
-    menu_button_frame = tk.Frame(window, width=150, height=110, bd=0)
+    menu_button_frame = tk.Frame(window, width=0.078*window.winfo_screenwidth(), height=0.102*window.winfo_screenheight(), bd=0)
     menu_button_frame.place(anchor='s', relx=0.05, rely=0.12)
     menu_button_frame.pack_propagate(False)
 
-    menu_font=font.Font(family='Chaparral Pro', size=25,weight='bold')
+    menu_font=font.Font(family='Chaparral Pro', size=int(0.013*window.winfo_screenwidth()),weight='bold')
     menu_button = tk.Button(menu_button_frame, text='◄\nMENU', font=menu_font, width=100, height=100, wraplength=130, bg='#424649', fg="white", borderwidth=5)
     menu_button.pack()
 
-    nome_frame = tk.Frame(window, width=800, height=80, bg='#222426')
+    nome_frame = tk.Frame(window, width=0.4170*window.winfo_screenwidth(), height=0.074*window.winfo_screenheight(), bg='#222426')
     nome_frame.place(anchor='s', relx=0.5, rely=0.72)
     nome_frame.pack_propagate(False)
 
-    nome_font=font.Font(family='Chaparral Pro',weight='bold',size=30)
+    nome_font=font.Font(family='Chaparral Pro',weight='bold',size=int(0.015*window.winfo_screenwidth()))
     nome = tk.Label(nome_frame, text=vet.getNome(), font=nome_font, justify='center', padx=0.5, pady=0.5, fg='white', bg='#222426')
     nome.pack()
 
     
-    options = tk.Frame(window, width=800, height=200, bg='#222426')
+    options = tk.Frame(window, width=0.417*window.winfo_screenwidth(), height=0.185*window.winfo_screenheight(), bg='#222426')
     options.pack()
     options.place(anchor='s', relx=0.5, rely=0.9)
 
@@ -218,19 +217,19 @@ def createInstrumentQuizLayout(vet, instruments, full):
     options.grid_rowconfigure(0, weight = 1)
     options.grid_rowconfigure(1, weight = 1)
 
-    top_left_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    top_left_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     top_left_option.grid(column=0, row=0)
     top_left_option.pack_propagate(False)
 
-    top_right_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    top_right_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     top_right_option.grid(column=1, row=0)
     top_right_option.pack_propagate(False)
 
-    bottom_left_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    bottom_left_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     bottom_left_option.grid(column=0, row=1)
     bottom_left_option.pack_propagate(False)
 
-    bottom_right_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    bottom_right_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     bottom_right_option.grid(column=1, row=1)
     bottom_right_option.pack_propagate(False)
 
@@ -314,8 +313,8 @@ def iterationName(vets):
     createNameQuizLayout(v, options, vets)
 
 def initialScreen(vets):
-    myFont = font.Font(family='Chaparral Pro', size=18, weight='bold')
-    logo = tk.Frame(window, width=250, height=250)
+    myFont = font.Font(family='Chaparral Pro', size=int(0.009*window.winfo_screenwidth()), weight='bold')
+    logo = tk.Frame(window, width=0.13*window.winfo_screenwidth(), height=0.231*window.winfo_screenheight())
     logo.pack()
     logo.place(anchor='n', relx=0.5, rely=0.02)
 
@@ -330,7 +329,7 @@ def initialScreen(vets):
     label.pack_propagate(False)
     label.pack()
 
-    options = tk.Frame(window, width=320, height=700, bg="#222426")
+    options = tk.Frame(window, width=0.167*window.winfo_screenwidth(), height=0.648*window.winfo_screenheight(), bg="#222426")
     options.pack()
     options.place(anchor='n', relx=0.5, rely=0.27)
 
@@ -343,57 +342,57 @@ def initialScreen(vets):
     options.grid_rowconfigure(4, weight = 1)
     options.grid_rowconfigure(5, weight = 1)
 
-    first_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    first_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     first_option.grid(column=0, row=0)
     first_option.pack_propagate(False)
 
-    second_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    second_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     second_option.grid(column=0, row=1)
     second_option.pack_propagate(False)
 
-    third_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    third_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     third_option.grid(column=0, row=2)
     third_option.pack_propagate(False)
 
-    fourth_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    fourth_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     fourth_option.grid(column=0, row=3)
     fourth_option.pack_propagate(False)
 
-    fifth_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    fifth_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     fifth_option.grid(column=0, row=4)
     fifth_option.pack_propagate(False)
 
-    sixth_option = tk.Frame(options,width=380, height=90, bg="#222426")
+    sixth_option = tk.Frame(options,width=0.198*window.winfo_screenwidth(), height=0.083*window.winfo_screenheight(), bg="#222426")
     sixth_option.grid(column=0, row=5)
     sixth_option.pack_propagate(False)
 
     option1 = tk.Button(first_option,text="NOMES E ALCUNHAS",
-                        font=myFont, wraplength=350, justify='center',
+                        font=myFont, wraplength=0.198*window.winfo_screenwidth(), justify='center',
                         width=100, height=100, bd=0, bg='#424649', fg="white",
                         borderwidth=10)
     option1.pack()
     option2 = tk.Button(second_option, text="INSTRUMENTOS",
-                        font=myFont, wraplength=350, justify='center',
+                        font=myFont, wraplength=0.198*window.winfo_screenwidth(), justify='center',
                         width=100, height=100,  bd=0,bg="#424649", fg="white",
                         borderwidth=10)
     option2.pack()
     option3 = tk.Button(third_option,text="Coming Soon...", 
-                        font=myFont, wraplength=350, justify='center',
+                        font=myFont, wraplength=0.198*window.winfo_screenwidth(), justify='center',
                         width=100,height=100,  bd=0,bg="#424649", fg="white",
                         borderwidth=10)
     option3.pack()
     option4 = tk.Button(fourth_option, text="Coming Soon...",
-                        font=myFont, wraplength=350, justify='center', 
+                        font=myFont, wraplength=0.198*window.winfo_screenwidth(), justify='center', 
                         width=100,height=100, bd=0, bg="#424649", fg="white",
                         borderwidth=10)
     option4.pack()
     option5 = tk.Button(fifth_option,text="Coming Soon...", 
-                        font=myFont, wraplength=350, justify='center',
+                        font=myFont, wraplength=0.198*window.winfo_screenwidth(), justify='center',
                         width=100,height=100,  bd=0,bg="#424649", fg="white",
                         borderwidth=10)
     option5.pack()
     option6 = tk.Button(sixth_option, text="Coming Soon...",
-                        font=myFont, wraplength=350, justify='center', 
+                        font=myFont, wraplength=0.198*window.winfo_screenwidth(), justify='center', 
                         width=100,height=100, bd=0, bg="#424649", fg="white",
                         borderwidth=10)
     option6.pack()
@@ -408,7 +407,7 @@ base_folder = os.path.dirname(__file__)
 window = tk.Tk()
 window.title("Who is that Veterano?")
 window.configure(width=window.winfo_screenwidth(), height=window.winfo_screenheight(), bg='#222426')
-
+print(window.winfo_screenwidth(), window.winfo_screenheight())
 initialScreen(vets)
 
 window.mainloop()
